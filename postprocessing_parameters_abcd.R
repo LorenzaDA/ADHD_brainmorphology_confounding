40ae7cdcbe3213409754f0f7d3b19bebca6d6bab
Sander Lamballais <lamballais.sander@gmail.com>
2022-09-23 20:33:45 +0200

    Update postprocessing_parameters_abcd.R

--- postprocessing_parameters_abcd.R
+++ postprocessing_parameters_abcd.R
@@ -26,19 +26,20 @@ labs <- lapply(seq_along(lut), function(i)
 
 #####################
 
-project_date <- "0304"
+project_date <- "150722"
 hemis <- c("lh","rh")
 det <- "cbcl_scr_syn_attention_r"
-out <- c("qdecr_area", "qdecr_volume")
+out <- c("qdecr_area", "qdecr_volume", "qdecr_thickness")
 
 ### QDECR MODELS ####
 
 # change according to confounders
-conf1 <- c("race_ethnicity","sex_at_birth","interview_age")
-conf2 <- c(conf1,"high.educ","household.income","devhx_3_age_at_birth_mother_p")
+conf1 <- c("race_ethnicity", "sex_at_birth", "interview_age")
+conf2 <- c(conf1,"high.educ", "household.income", "devhx_3_age_at_birth_mother_p")
 conf3 <- c(conf2, "devhx_8_tobacco_p", "devhx_8_marijuana_p", "asr_scr_totprob_r")
-conf5 <- c(conf3, "pea_wiscv_tss")
-conf <- list(conf1, conf2, conf3, conf4)
+conf4 <- c(conf3, "cbcl_scr_syn_aggressive_r")
+conf5 <- c(conf3, "euler_number")
+conf <- list(conf1, conf2, conf3, conf4, conf5)
 base_f <- lapply(conf, function(x) paste("~", det, "+", paste(x, collapse = "+")))
 f <- lapply(base_f, function(x) paste(out, x))
 
@@ -46,21 +47,3 @@ out2 <- sub("qdecr_", "", out)
 base_name <- paste0("_", project_date, ".", out2)
 
 m <- length(f) # number of models
-paths_lh <- paths_rh <- list()
-
-for (i in seq_along(out2)) {
-  project_names <- expand.grid(".M", seq_len(m), base_name[i]) %>%
-    apply(1, paste, collapse = "")
-  
-  project_names[m] <- paste0(".M", m, ".", out2[i])
-  
-  projects_lh <- paste0("lh", project_names)
-  projects_rh <- paste0("rh", project_names)
-  
-  mid_dir <- if (out2[i] == "area") "dir_out_area" else "dir_out_volume"
-  mid_dir <- rep(mid_dir, m)
-  mid_dir[m] <- "dir_out"
-  
-  paths_lh[[i]] <- file.path(dir_wd, mid_dir, projects_lh)
-  paths_rh[[i]] <- file.path(dir_wd, mid_dir, projects_rh)
-}
